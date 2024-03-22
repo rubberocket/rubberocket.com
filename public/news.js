173b08df2421f1b3459c1c8e5d52254b8a97a65e
Aleph Diallo <alephdiallo@proton.me>
2024-03-21 22:09:20 -0400

    ffffuck

--- public/news.js
+++ public/news.js
@@ -1,4 +1,4 @@
 var newsInfo = "2024/03/21: A demonic figure is attacking the city! Former Marines turned exorcists have come in to exorcise it. TWO presidents impeached in the span of 8 years! Among other things.";
 var newsMarquee = "<marquee>" + newsInfo + "</marquee>";
 
-document.getElementById("nMarq") = newsMarquee;
+document.getElementById("nMarq").innerHTML = newsMarquee;
