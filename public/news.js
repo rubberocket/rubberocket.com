ce160a0f60d9d4ef6bb70d7770f56fa61a23e251
Aleph Diallo <alephdiallo@proton.me>
2024-03-21 22:07:11 -0400

    my sister is annoying

--- public/news.js
+++ public/news.js
@@ -1,19 +1,2 @@
-function iniFrame() {
-    if (window.location !== window.parent.location) {
-        // The page is in an iFrame
-        return true;
-    }
-    else {
-        // The page is not in an iFrame
-        return false;
-    }
-}
-
 var newsInfo = "2024/03/21: A demonic figure is attacking the city! Former Marines turned exorcists have come in to exorcise it. TWO presidents impeached in the span of 8 years! Among other things.";
 var newsMarquee = "<marquee>" + newsInfo + "</marquee>";
-
-if (iniFrame() = true) {
-    document.write(newsMarquee);
-} else {
-    document.getElementById("nMarq").innerHTML = newsMarquee;
-}
