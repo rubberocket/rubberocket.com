fe0b0e5cdd589c9e9e18e3681afd965461fb19c6
Aleph Diallo <alephdiallo@proton.me>
2024-03-21 22:11:33 -0400

    :|

--- public/news.js
+++ public/news.js
@@ -1,3 +1,16 @@
 var newsInfo = "2024/03/21: A demonic figure is attacking the city! Former Marines turned exorcists have come in to exorcise it. TWO presidents impeached in the span of 8 years! Among other things.";
 
-document.getElementById("nMarq").innerHTML = "<marquee>" + newsInfo + "</marquee>";
+
+function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+if (inIframe()) {
+    document.write("<marquee>" + newsInfo + "</marquee>");
+} else {
+    document.getElementById("nMarq").innerHTML = "<marquee>" + newsInfo + "</marquee>";
+}
