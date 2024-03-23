ea0959cd7de6bf80027bbfb5485351cc2e9abecb
Aleph Diallo <alephdiallo@proton.me>
2024-03-23 13:29:58 -0400

    efficiency

--- public/mystuff/videos/videos.js
+++ public/mystuff/videos/videos.js
@@ -1,27 +1,30 @@
-function loady(reqvid){
+const urlParams = new URLSearchParams(queryString);
+let reqvid = urlParams.get('video')
 
     if (reqvid === "ballbounce81224") {
         document.getElementById("ballbounce81224").style = "display: block";
+        window.location.href = "#ballbounce81224";
     } else {
         document.getElementById("ballbounce81224").style = "display: none";
     }
 
     if (reqvid === "mcream") {
         document.getElementById("mcream").style = "display: block";
+        window.location.href = "#mcream";
     } else {
         document.getElementById("mcream").style = "display: none";
     }
 
     if (reqvid === "toss1e1") {
         document.getElementById("toss1e1").style = "display: block";
+        window.location.href = "#toss1e1";
     } else {
         document.getElementById("toss1e1").style = "display: none";
     }
 
     if (reqvid === "scribbletrail") {
         document.getElementById("scribbletrail").style = "display: block";
+        window.location.href = "#scribbletrail";
     } else {
         document.getElementById("scribbletrail").style = "display: none";
     }
-
-}
