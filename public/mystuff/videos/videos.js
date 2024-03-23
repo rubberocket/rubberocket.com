d40531b0f886c57e5d97d7e629d22162ed494e47
Aleph Diallo <alephdiallo@proton.me>
2024-03-23 13:15:00 -0400

    >:|

--- public/mystuff/videos/videos.js
+++ public/mystuff/videos/videos.js
@@ -1,9 +1,17 @@
 function loady(reqvid, license){
 
+return reqvid;
+return license;
+
+function doNice() {
+    return "<br> <div id='thething'> <embed src='" + reqvid + "' class='viddy'></embed><br><strong>License</strong>:" + license + " </div>";
+}
+
 if (document.getElementById("thething")) {
     document.getElementById("thething").remove();
+    this.innerHTML += doNice();
 } else {
-    this.innerHTML += "<br> <div id='thething'> <embed src='" + reqvid + "' class='viddy'></embed><br><strong>License</strong>:" + license + " </div>";
+    this.innerHTML += doNice();
 }
 
 }
