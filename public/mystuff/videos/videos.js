174d21ecba645de8cd08cc6acda99dd565f71788
Aleph Diallo <alephdiallo@proton.me>
2024-03-23 13:38:39 -0400

    cool

--- public/mystuff/videos/videos.js
+++ public/mystuff/videos/videos.js
@@ -5,29 +5,35 @@ const urlParams = new URLSearchParams(queryString);
 let reqvid = urlParams.get('video')
 
     if (reqvid === "ballbounce81224") {
-        document.getElementById("ballbounce81224").style = "display: block";
+        document.getElementById("ballbounce81224").style = "display: block;";
         window.location.href = "#ballbounce81224";
     } else {
-        document.getElementById("ballbounce81224").style = "display: none";
+        document.getElementById("ballbounce81224").style = "display: none;";
+    }
+
+    if (reqvid === "killafish") {
+        document.getElementById("killafish").style = "display: block;";
+    } else {
+        document.getElementById("killafish").style = "display: none;";
     }
 
     if (reqvid === "mcream") {
-        document.getElementById("mcream").style = "display: block";
+        document.getElementById("mcream").style = "display: block;";
         window.location.href = "#mcream";
     } else {
-        document.getElementById("mcream").style = "display: none";
+        document.getElementById("mcream").style = "display: none;";
     }
 
     if (reqvid === "toss1e1") {
-        document.getElementById("toss1e1").style = "display: block";
+        document.getElementById("toss1e1").style = "display: block;";
         window.location.href = "#toss1e1";
     } else {
-        document.getElementById("toss1e1").style = "display: none";
+        document.getElementById("toss1e1").style = "display: none;";
     }
 
     if (reqvid === "scribbletrail") {
-        document.getElementById("scribbletrail").style = "display: block";
+        document.getElementById("scribbletrail").style = "display: block;";
         window.location.href = "#scribbletrail";
     } else {
-        document.getElementById("scribbletrail").style = "display: none";
+        document.getElementById("scribbletrail").style = "display: none;";
     }
