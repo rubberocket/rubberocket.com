399cffdb79ad47f709d452d341ba66826cd07665
Aleph Diallo <alephdiallo@proton.me>
2024-03-24 13:59:42 -0400

    addLisener

--- public/mystuff/videos/videos.js
+++ public/mystuff/videos/videos.js
@@ -1,47 +1,48 @@
 /* get our video */
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-let reqvid = urlParams.get('video')
-
-    if (reqvid === "guypsycho") {
-        document.getElementById("guypsycho").style = "display: block;";
-        window.location.href = "#guypsycho";
-    } else {
-        document.getElementById("guypsycho").style = "display: none;";
-    }
-
-    if (reqvid === "ballbounce81224") {
-        document.getElementById("ballbounce81224").style = "display: block;";
-        window.location.href = "#ballbounce81224";
-    } else {
-        document.getElementById("ballbounce81224").style = "display: none;";
-    }
-
-    if (reqvid === "killafish") {
-        document.getElementById("killafish").style = "display: block;";
-        window.location.href = "#killafish";
-    } else {
-        document.getElementById("killafish").style = "display: none;";
-    }
-
-    if (reqvid === "mcream") {
-        document.getElementById("mcream").style = "display: block;";
-        window.location.href = "#mcream";
-    } else {
-        document.getElementById("mcream").style = "display: none;";
-    }
-
-    if (reqvid === "toss1e1") {
-        document.getElementById("toss1e1").style = "display: block;";
-        window.location.href = "#toss1e1";
-    } else {
-        document.getElementById("toss1e1").style = "display: none;";
-    }
-
-    if (reqvid === "scribbletrail") {
-        document.getElementById("scribbletrail").style = "display: block;";
-        window.location.href = "#scribbletrail";
-    } else {
-        document.getElementById("scribbletrail").style = "display: none;";
-    }
+window.addEventListener("load", (event) => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let reqvid = urlParams.get('video')
+    
+        if (reqvid === "guypsycho") {
+            document.getElementById("guypsycho").style = "display: block;";
+            window.location.href = "#guypsycho";
+        } else {
+            document.getElementById("guypsycho").style = "display: none;";
+        }
+    
+        if (reqvid === "ballbounce81224") {
+            document.getElementById("ballbounce81224").style = "display: block;";
+            window.location.href = "#ballbounce81224";
+        } else {
+            document.getElementById("ballbounce81224").style = "display: none;";
+        }
+    
+        if (reqvid === "killafish") {
+            document.getElementById("killafish").style = "display: block;";
+            window.location.href = "#killafish";
+        } else {
+            document.getElementById("killafish").style = "display: none;";
+        }
+    
+        if (reqvid === "mcream") {
+            document.getElementById("mcream").style = "display: block;";
+            window.location.href = "#mcream";
+        } else {
+            document.getElementById("mcream").style = "display: none;";
+        }
+    
+        if (reqvid === "toss1e1") {
+            document.getElementById("toss1e1").style = "display: block;";
+            window.location.href = "#toss1e1";
+        } else {
+            document.getElementById("toss1e1").style = "display: none;";
+        }
+    
+        if (reqvid === "scribbletrail") {
+            document.getElementById("scribbletrail").style = "display: block;";
+            window.location.href = "#scribbletrail";
+        } else {
+            document.getElementById("scribbletrail").style = "display: none;";
+        } 
+});
---
+++ public/randomimage.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", (event) => {
+    var randomImage = new Array("assets/multimedia/images/rand/gmaine.png");
+    var imageNum = Math.floor(Math.random()*randomImage.length);
+    document.getElementById("gmane").src = (piss[random404]);
+});
