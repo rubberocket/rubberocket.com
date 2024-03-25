d13d9776614158584295ce2c75fed89bee65e471
Aleph Diallo <alephdiallo@proton.me>
2024-03-25 00:11:36 -0400

    mane

--- public/randomimage.js
+++ public/randomimage.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", (event) => {
-    var randomImage = new Array("assets/multimedia/images/rand/gmaine.png");
+    var randomImage = new Array("assets/multimedia/images/rand/gmaine.png", "assets/multimedia/images/rand/shmib.png");
     var imageNum = Math.floor(Math.random()*randomImage.length);
     document.getElementById("gmane").src = (piss[random404]);
 });
