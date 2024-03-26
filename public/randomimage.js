a3130dfbd61a8756af8bdf81714dc0ec89650450
Aleph Diallo <alephdiallo@proton.me>
2024-03-25 22:00:43 -0400

    whoops?

--- public/randomimage.js
+++ public/randomimage.js
@@ -3,5 +3,5 @@ window.addEventListener("load", (event) => {
     var randomImageAlt = new Array("the gman is real!!!!", "shmibby the idiot");
     var imageNum = Math.floor(Math.random()*randomImage.length); // we don't need one for the alts
     document.getElementById("gmane").src = (randomImage[imageNum]);
-    document.getElementById("gmane").src = (randomImageAlt[imageNum]);
+    document.getElementById("gmane").alt = (randomImageAlt[imageNum]);
 });
