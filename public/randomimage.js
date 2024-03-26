22d614311ff6b82da04c78168b24f496052c4f47
Aleph Diallo <alephdiallo@proton.me>
2024-03-25 21:58:43 -0400

    w3c bawwing about nothing as usual i see

--- public/randomimage.js
+++ public/randomimage.js
@@ -1,5 +1,7 @@
 window.addEventListener("load", (event) => {
     var randomImage = new Array("assets/multimedia/images/rand/gmaine.png", "assets/multimedia/images/rand/shmib.png");
-    var imageNum = Math.floor(Math.random()*randomImage.length);
+    var randomImageAlt = new Array("the gman is real!!!!", "shmibby the idiot");
+    var imageNum = Math.floor(Math.random()*randomImage.length); // we don't need one for the alts
     document.getElementById("gmane").src = (randomImage[imageNum]);
+    document.getElementById("gmane").src = (randomImageAlt[imageNum]);
 });
